660469ea6bf7013af1e40a3a30cb8007808ec2d5
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-01-31 00:55:18 +0000

    Add more test inputs

--- sql_scripts/test_input.sql
+++ sql_scripts/test_input.sql
@@ -11,3 +11,9 @@ INSERT INTO skip_times
 INSERT INTO skip_times
   VALUES (DEFAULT, 14719, 2, 'crunchyroll', 'ed', DEFAULT, 1320, 1380, 1440, DEFAULT, gen_random_uuid ());
 
+INSERT INTO skip_times
+  VALUES (DEFAULT, 39783, 1, 'aniwatch', 'op', DEFAULT, 27, 117, 1450.551803, DEFAULT, gen_random_uuid ());
+
+INSERT INTO skip_times
+  VALUES (DEFAULT, 39783, 1, 'aniwatch', 'ed', DEFAULT, 1355, 1450, 1450.551803, DEFAULT, gen_random_uuid ());
+
