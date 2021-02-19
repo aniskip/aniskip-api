d0397550ee0294f1af89afe48eaf716696f73277
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-02-19 12:40:36 +0000

    Add backup database shell script

---
+++ sql_scripts/backup_database.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ./docker
+docker-compose exec db pg_dump -U postgres --table=skip_times --data-only db > ../sql_scripts/opening-skipper-database-backup/db.dump
+
+cd ../sql_scripts/opening-skipper-database-backup
+git commit -am "Update database backup"
+git push
