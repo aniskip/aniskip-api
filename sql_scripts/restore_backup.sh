fadf590258f363a0050933c76db5f61aecaac70e
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-05-14 15:44:11 +0100

    Remove whitespace

--- sql_scripts/restore_backup.sh
+++ sql_scripts/restore_backup.sh
@@ -1,8 +1,8 @@
 !/bin/bash                                                                     
-                                                                                
+
 cd "${BASH_SOURCE%/*}"
 
-source .env                                                                     
-                                                                                
+source .env
+
 docker exec aniskip_db_1 psql -U postgres -d db -c 'TRUNCATE skip_times'
 curl https://$GITHUB_PAT@raw.githubusercontent.com/lexesjan/aniskip-database-backup/main/db.dump | docker exec -i aniskip_db_1 psql -v ON_ERROR_STOP=1 -U postgres db
