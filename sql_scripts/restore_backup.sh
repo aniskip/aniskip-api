ec8170700f50e57f333eeda5ae9b61c304551c7c
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-05-15 03:23:28 +0100

    Fix shebang

--- sql_scripts/restore_backup.sh
+++ sql_scripts/restore_backup.sh
@@ -1,4 +1,4 @@
-!/bin/bash                                                                     
+#!/bin/bash                                                                     
 
 cd "${BASH_SOURCE%/*}"
 
