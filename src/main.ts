9546c1907c474d5bb7130566859142271b170eaa
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-08-26 23:59:24 +0100

    Trust proxy

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableVersioning();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal']);
   app.enableCors({ origin: '*' });
   app.use(helmet());
   await app.listen(5000);
