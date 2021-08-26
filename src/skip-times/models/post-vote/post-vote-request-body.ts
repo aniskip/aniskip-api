473db1ce6cf404dc35d2943aa5ca6b5debe3e7aa
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-08-26 10:28:36 +0100

    Rename files

--- src/skip-times/models/post-vote/post-vote-request-body.ts
+++
@@ -1,7 +0,0 @@
-import { IsIn } from 'class-validator';
-import { VoteType } from '../../skip-times.types';
-
-export class PostVoteRequestBody {
-  @IsIn(['upvote', 'downvote'])
-  vote_type!: VoteType;
-}
--- src/skip-times/models/post-vote/post-vote-request-params.ts
+++
@@ -1,6 +0,0 @@
-import { IsUUID } from 'class-validator';
-
-export class PostVoteRequestParams {
-  @IsUUID()
-  skip_id!: string;
-}
