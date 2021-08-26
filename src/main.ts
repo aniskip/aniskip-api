1ba347b06db4d627fc9fe2b9930374e10207c583
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-08-26 23:54:54 +0100

    Add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(helmet());
   await app.listen(5000);
 }
 bootstrap();
