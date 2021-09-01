2e852cfef237fdcee2deecbc3c9216be9cf2575e
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-09-01 21:30:29 +0100

    Remove skip times repository export

--- src/skip-times/index.ts
+++ src/skip-times/index.ts
@@ -1,5 +1,4 @@
 export * from './skip-times.controller';
 export * from './skip-times.module';
-export * from '../repositories/skip-times.repository';
 export * from './skip-times.service';
 export * from './skip-times.types';
