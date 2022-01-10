6f1da5c6d2908306dac55fbe186c96767fcc2209
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2022-01-10 11:11:05 +0000

    Fix integration tests

--- src/relation-rules/__tests__/relation-rules.controller.v2.integration.test.ts
+++ src/relation-rules/__tests__/relation-rules.controller.v2.integration.test.ts
@@ -39,16 +39,11 @@ describe('RelationRulesControllerV2', () => {
           found: true,
           rules: [
             {
-              from: {
-                start: 60,
-                end: 75,
-              },
-              to: {
-                malId: 40028,
-                start: 1,
-                end: 16,
-              },
+              from: { start: 60, end: 75 },
+              to: { malId: 40028, start: 1, end: 16 },
             },
+            { from: { start: 17 }, to: { malId: 48583, start: 1 } },
+            { from: { start: 76 }, to: { malId: 48583, start: 1 } },
           ],
         })
         .expect(HttpStatus.OK, done);
